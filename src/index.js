ec5dfcf239738e556cf29616876a4d40aefb7b73
Shannqa <116514532+Shannqa@users.noreply.github.com>
2023-04-10 11:57:52 +0200

    Add theme code

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import icon from "./light_mode_FILL0_wght400_GRAD0_opsz48.png";
+
+function setTheme() {
+  const themeIcon = new Image();
+  themeIcon.src = icon;
+  themeIcon.classList.add("");
+  const root = document.documentElement;
+  const newTheme = root.className === "dark" ? "light" : "dark";
+  root.className = newTheme;
+}
+document.querySelector(".theme-toggle").addEventListener("click", setTheme);
+
+export default setTheme;
